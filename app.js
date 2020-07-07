719e729c4f55a7e25247d9c405e42eba5cbe6ed8
harriet-the-spy <dpcairns@gmail.com>
2020-07-07 10:44:45 -0700

    add event listener

---
+++ app.js
@@ -0,0 +1,10 @@
+// import functions and grab DOM elements
+const myButton = document.querySelector('#button');
+
+// initialize state
+let jellybeans = 0;
+
+// set event listeners to update state and DOM
+myButton.addEventListener('click', () => {
+    console.log('hello');
+});
